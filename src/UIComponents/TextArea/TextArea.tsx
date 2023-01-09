01ac65c4a8e3323c29d2e56120687a617f4dfd55
LoisChen68 <a8283626@gmail.com>
2023-01-10 03:00:34 +0800

    feat: add TextArea Component

---
+++ src/UIComponents/TextArea/TextArea.tsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useRef, useState } from 'react'
+import style from './TextArea.module.scss'
+
+interface textAreaProps {
+  placeholder: string
+  scrollHeight: number
+}
+
+export function TextArea(props: textAreaProps) {
+  const [value, setValue] = useState('')
+  const textAreaRef = useRef<HTMLTextAreaElement>(null)
+
+  autoSizeTextArea(textAreaRef.current, value, props.scrollHeight)
+
+  function handleChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    const value = e.target.value
+
+    setValue(value)
+  }
+
+  return (
+    <textarea
+      className={`${style['textarea']} ${style['scrollbar']}`}
+      onChange={handleChange}
+      placeholder={props.placeholder}
+      ref={textAreaRef}
+      value={value}
+    />
+  )
+}
+
+// 讓 textArea 根據輸入行數自動調整高度
+function autoSizeTextArea(
+  textAreaRef: HTMLTextAreaElement | null,
+  value: string,
+  height: number
+) {
+  useEffect(() => {
+    if (textAreaRef) {
+      // 定義 textArea 預設高度
+      textAreaRef.style.height = ''
+      // 定義 textArea 元素內容高度
+      const scrollHeight = textAreaRef.scrollHeight
+      if (scrollHeight < height) {
+        // 當 scrollHeight 小於 height 隱藏內容
+        textAreaRef.style.overflow = 'hidden'
+        // 將 textArea 高度依據 scrollHeight 進行變化
+        textAreaRef.style.height = `${scrollHeight}px`
+      } else {
+        // 當 scrollHeight 大於 height 顯示內容
+        textAreaRef.style.overflow = ''
+        // 當 scrollHeight 大於 height 將文本區域高度固定在 height px
+        textAreaRef.style.height = `${height}px`
+      }
+    }
+  }, [textAreaRef, value])
+}
