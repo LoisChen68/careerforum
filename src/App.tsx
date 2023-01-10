ad2bc75a99b0c13446b61ba693d067c774180681
LoisChen68 <a8283626@gmail.com>
2023-01-11 04:20:26 +0800

    feat: add QuestionPage Route & modify App.scss

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom'
 import './App.scss'
 import Layout from './Pages/Layout/Layout'
 import ForumHome from './Pages/ForumHome/ForumHome'
+import QuestionPage from './Pages/QuestionPage/QuestionPage'
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/career-forum" element={<Layout />}>
+      <Route path="/careerforum" element={<Layout />}>
         <Route path="home" element={<ForumHome />} />
+        <Route path=":id" element={<QuestionPage />} />
       </Route>
       <Route path="/*" element={<Layout />}></Route>
     </Routes>
---
+++ src/Pages/QuestionPage/QuestionPage.tsx
@@ -0,0 +1,4 @@
+
+export default function QuestionPage() {
+  return <h1>問題頁</h1>
+}
