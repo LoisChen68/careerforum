0035c0e4be829d52b4dded4a0da4bb5f1a9da7f3
LoisChen68 <a8283626@gmail.com>
2023-01-11 04:24:39 +0800

    modify: modify Header Component

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import Button from '../../UIComponents/Button/Button'
+import UserAvatar from '../../UIComponents/UserAvatar/UserAvatar'
 import style from './Header.module.scss'
 
 interface headerProps {
@@ -13,10 +14,15 @@ export default function Header(props: headerProps) {
   return (
     <header className={style['header']}>
       <div className={style['header-wrapper']}>
-        <div className={style['header-logo']}>
+        <div className={style['header-nav']}>
           <Link to="/">
             <h3 className={style['header-title']}>Career Forum</h3>
           </Link>
+          <div className={style['nav-link']}>
+            <Link to="/careerforum/home">
+              <nav className={style['nav-item']}>問答版</nav>
+            </Link>
+          </div>
         </div>
         <div className={style['header-button']}>
           {!props.authPass && (
@@ -38,16 +44,25 @@ export default function Header(props: headerProps) {
             </>
           )}
           {props.authPass && (
-            <Button
-              type="button"
-              innerText="登出"
-              style="button-login"
-              onClick={props.onLogoutClick}
-              disabled={false}
+            <UserAvatar
+              userAvatar={currentUser.avatar}
+              avatarStyle={'header-user-avatar'}
             />
           )}
         </div>
       </div>
     </header>
   )
+}
+
+const currentUser = {
+  id: '1', // user PK:id
+  role: 'TA',
+  name: '', // 可填可不填
+  account: 'user1',
+  email: 'user1@careerForum.com',
+  avatar: 'https://cdn-icons-png.flaticon.com/512/1864/1864514.png',
+  cover: 'http://...',
+  createdAt: '2023/01/07',
+  updatedAt: '2023/01/07',
 }
