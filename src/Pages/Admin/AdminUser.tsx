d4475030471eb0dfda771447cb60c32ca78ba6f4
GinoHsu <ginohsu16@gmail.com>
2023-01-20 23:25:29 +0800

    feat: add alert when approval change in AdminUser

--- src/Pages/Admin/AdminUser.tsx
+++ src/Pages/Admin/AdminUser.tsx
@@ -2,9 +2,10 @@
 import { useState, useEffect } from 'react'
 import adminAPI from '../../request/API/adminAPI'
 import { useRender } from '../../Contexts/RenderContext'
-import style from './AdminUser.module.scss'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import ButtonLoader from '../../UIComponents/ButtonLoader/ButtonLoader'
+import UserAvatar from '../../UIComponents/UserAvatar/UserAvatar'
+import style from './AdminUser.module.scss'
 
 interface user {
   id: number
@@ -29,6 +30,9 @@ export default function AdminUser() {
   const [limit, setLimit] = useState(3)
   const [hasMore, setHasMore] = useState(true)
   const [loading, setLoading] = useState(true)
+  const [alert, setAlert] = useState(false)
+  const [approval, setApproval] = useState('')
+  const [userIdIsClicked, setUserIdIsClicked] = useState(0)
   const render = useRender()
 
   useEffect(() => {
@@ -47,7 +51,7 @@ export default function AdminUser() {
         }
       })
       .catch((err) => console.error(err))
-  })
+  }, [render?.isRender])
 
   // Lazy Loading for users
   const changePage = () => {
@@ -59,6 +63,30 @@ export default function AdminUser() {
         setUsers(users.concat(usersData))
         setPage((page) => page + 1)
         setLimit((limit) => limit + 3)
+      })
+      .catch((err) => console.error(err))
+  }
+
+  const handleOnClick = (userId: number, status: string) => {
+    setAlert(true)
+    setApproval(status)
+    setUserIdIsClicked(userId)
+  }
+
+  const handleOnCancel = () => {
+    setAlert(false)
+    setApproval('')
+    setUserIdIsClicked(0)
+  }
+
+  const handleOnSure = () => {
+    adminAPI
+      .patchUsers(userIdIsClicked, approval)
+      .then(() => {
+        render?.handleRerender(true)
+        setAlert(false)
+        setApproval('')
+        setUserIdIsClicked(0)
       })
       .catch((err) => console.error(err))
   }
@@ -76,9 +104,10 @@ export default function AdminUser() {
           {users.map((user: user) => (
             <div key={user.id} className={style['user']}>
               <div className={style['user-header']}>
-                <div className={style['avatar']}>
-                  <img src={user.avatar} alt="使用者頭像" />
-                </div>
+                <UserAvatar
+                  userAvatar={user.avatar}
+                  avatarStyle={'body-user-avatar'}
+                />
                 <div className={style['appellation']}>
                   <p>{user.account}</p>
                   <p className={style['appellation-role']}>{user.role}</p>
@@ -96,8 +125,18 @@ export default function AdminUser() {
                 </div>
                 <div className={style['approvalStatus']}>
                   <p>{`審查狀態：${user.approvalStatus}`}</p>
-                  <button className={style['btn']}>Approved</button>
-                  <button className={style['btn-warning']}>Rejected</button>
+                  <button
+                    className={style['btn']}
+                    onClick={() => handleOnClick(user.id, 'approved')}
+                  >
+                    Approved
+                  </button>
+                  <button
+                    className={style['btn-warning']}
+                    onClick={() => handleOnClick(user.id, 'rejected')}
+                  >
+                    Rejected
+                  </button>
                 </div>
               </div>
             </div>
@@ -105,6 +144,22 @@ export default function AdminUser() {
         </InfiniteScroll>
         {usersStatus === 'nothing' && <p>目前還沒有任何使用者</p>}
       </div>
+      {alert && (
+        <>
+          <div className={style['back-drop']} onClick={handleOnCancel} />
+          <div className={style['alert-container']}>
+            <h3>{`確定要更改使用者狀態為 ${approval}？`}</h3>
+            <div className={style['buttons']}>
+              <button className={style['btn-cancel']} onClick={handleOnCancel}>
+                取消
+              </button>
+              <button className={style['btn-sure']} onClick={handleOnSure}>
+                確定
+              </button>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   )
 }
