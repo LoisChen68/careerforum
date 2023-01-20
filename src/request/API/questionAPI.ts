624935a4acf62429fa5a054e8ec22ef33152b29a
GinoHsu <ginohsu16@gmail.com>
2023-01-20 16:22:51 +0800

    style: run prettier

--- src/request/API/questionAPI.ts
+++ src/request/API/questionAPI.ts
@@ -8,18 +8,12 @@ export default {
     return api.get(`/questions/${id}/answers`)
   },
   postAnswers(id: number | undefined, content: string) {
-    return api.post(
-      `/questions/${id}/answers`,
-      { content }
-    )
+    return api.post(`/questions/${id}/answers`, { content })
   },
   getQuestions(page: number, limit: number) {
     return api.get(`/questions?page=${page}&limit=${limit}`)
   },
   postQuestion(title: string, content: string) {
-    return api.post(
-      '/questions',
-      { title, content }
-    )
+    return api.post('/questions', { title, content })
   },
 }
