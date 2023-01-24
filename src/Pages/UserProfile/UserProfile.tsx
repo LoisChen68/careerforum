6f3a7a550eecf63651252f542de74ea8afe4d2b5
LoisChen68 <a8283626@gmail.com>
2023-01-25 02:15:50 +0800

    feat: modify UserProfile add userSetting Link

--- src/Pages/UserProfile/UserProfile.tsx
+++ src/Pages/UserProfile/UserProfile.tsx
@@ -3,13 +3,15 @@ import userAPI from '../../request/API/userAPI'
 import { useParams } from 'react-router'
 import UserAvatar from '../../UIComponents/UserAvatar/UserAvatar'
 import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { useGetUser } from '../../Contexts/UserContext'
+import Button from '../../UIComponents/Button/Button'
 
 const defaultUserData = {
   id: 0,
   role: '',
   name: '',
   email: '',
-  account: '',
   avatar: '',
   cover: '',
   deletedAt: '',
@@ -19,6 +21,7 @@ const defaultUserData = {
 }
 
 export default function UserProfile() {
+  const getUser = useGetUser()
   const param = useParams()
   const [userData, setUserData] = useState(defaultUserData)
 
@@ -27,19 +30,36 @@ export default function UserProfile() {
       .getUser(Number(param.id))
       .then((res) => setUserData(res.data))
       .catch((err) => console.log(err))
-  }, [])
+  }, [Number(param.id)])
 
   return (
     <>
-      <div className={style['user-profile']}>
-        <div className={style['wrapper']}>
-          <div className={style['container']}>
+      <div className={style['wrapper']}>
+        <div className={style['container']}>
+          <div className={style['user-profile']}>
             <UserAvatar
-              avatarStyle="body-user-avatar"
+              avatarStyle="user-profile-avatar"
               userAvatar={userData.avatar}
             />
-            <p>{userData.name}</p>
-            <p>{userData.role}</p>
+            <div className={style['user-data']}>
+              <p>{userData.name}</p>
+              <p className={style['role']}>{userData.role}</p>
+            </div>
+            <div>
+              {getUser?.user.id === Number(param.id) && (
+                <Link to={`/careerForum/users/setting/${getUser?.user?.id}`} >
+                  <Button
+                    type="submit"
+                    style="user-setting-button"
+                    onClick={(e) => { e }}
+                    disabled={false}
+                  >
+                    <p>編輯個人檔案</p>
+                  </Button>
+                </Link>
+              )}
+
+            </div>
           </div>
         </div>
       </div>
