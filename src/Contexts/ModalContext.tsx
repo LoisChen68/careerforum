b1727f0b5842cef6c543cc087ad9a90528f61574
Lois Chen <107075349+LoisChen68@users.noreply.github.com>
2023-02-03 02:09:31 +0800

    Revert "Develop"

--- src/Contexts/ModalContext.tsx
+++ src/Contexts/ModalContext.tsx
@@ -31,7 +31,7 @@ export default function ModalContextProvider({
 
   const modalContextData: modalContextData = {
     handleSetModal,
-    modalStatus: modalStatus,
+    modalStatus,
   }
 
   return (
